ee2707f9fcd1231944b1556868448fce96355e79
Nikita Epshtein <{ID}+{username}@users.noreply.github.com>
2022-11-02 15:29:56 +0100

    Publish docker workflow

--- backend/start.js
+++ backend/start.js
@@ -1,12 +1,21 @@
 const proxy = require('cors-anywhere')
 
-const host = '0.0.0.0'
-const port = 9000
+const originWhitelist = process.env.NAVIG8_CORS_PROXY_WHITELIST ?
+    JSON.parse(process.env.NAVIG8_CORS_PROXY_WHITELIST) :
+    []
+const requireHeader = process.env.NAVIG8_CORS_PROXY_REQUIRED_HEADERS ?
+    JSON.parse(process.env.NAVIG8_CORS_PROXY_REQUIRED_HEADERS) :
+    ['origin', 'x-requested-with']
+
+// Some of https://cheatsheetseries.owasp.org/cheatsheets/HTTP_Headers_Cheat_Sheet.html
+const headersRemovedByDefault = ['cookie', 'cookie2', 'set-cookie', 'x-powered-by', 'x-aspnetmvc-version', 'x-dns-prefetch-control']
+const removeHeaders = process.env.NAVIG8_CORS_PROXY_FILTER_HEADERS ?
+    [...JSON.parse(process.env.NAVIG8_CORS_PROXY_FILTER_HEADERS), ...headersRemovedByDefault] :
+    headersRemovedByDefault
 
 proxy.createServer({
-    originWhitelist: [],
-    requireHeader: ['origin', 'x-requested-with'],
-    removeHeaders: ['cookie', 'cookie2']
-}).listen(port, host, function() {
-    console.log('Running CORS Anywhere on ' + host + ':' + port);
-});
+    originWhitelist,
+    requireHeader,
+    removeHeaders,
+    redirectSameOrigin: true,
+}).listen(9000, '0.0.0.0');
